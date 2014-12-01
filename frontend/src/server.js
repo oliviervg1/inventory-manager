d46533fe1c0548aba6efd60a8e3420f615794cd4
Olivier Van Goethem <olivier-vg@outlook.com>
2014-12-01 00:14:09 +0000

    Adding ability to add rooms and items

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,10 +1,13 @@
 var express = require('express'),
     ejs = require('ejs'),
+    bodyParser = require('body-parser'),
     backend = require('./lib/backend');
 
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
     backend.get('/rooms').then(
@@ -12,6 +15,54 @@ app.get('/', function(req, res) {
             res.render('index.ejs', {rooms: data});
         }
     ).done()
+});
+
+app.get('/rooms', function(req, res) {
+    res.render('rooms.ejs');
+});
+
+app.post('/rooms', function(req, res) {
+    backend.post('/rooms', {'name': req.body.name}).then(
+        function(data) {
+            res.redirect('/');
+        },
+        function(error) {
+            // TODO - add error handling
+            res.redirect('/');
+        }
+    ).done()
+});
+
+app.delete('/rooms', function(req, res) {
+    // TODO
+});
+
+app.get('/items', function(req, res) {
+    res.render('items.ejs');
+});
+
+app.post('/items', function(req, res) {
+
+    var endpoint = '/rooms/' + req.body.room + '/items/' + req.body.name;
+    var data = {
+        'description': req.body.description,
+        'weight': req.body.weight,
+        'is_fragile': req.body.is_fragile == 'on' ? true : false
+    };
+
+    backend.put(endpoint, data).then(
+        function(data) {
+            res.redirect('/');
+        },
+        function(error) {
+            // TODO - add error handling
+            res.redirect('/');
+        }
+    ).done()
+});
+
+app.delete('/items', function(req, res) {
+    // TODO
 });
 
 app.get('/manifest', function(req, res) {
